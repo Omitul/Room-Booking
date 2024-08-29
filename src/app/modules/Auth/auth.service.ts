9aaf5b48ecb0e1cc7ec147706497354959f8b421
Omitul <omitul1010@gmail.com>
2024-08-29 10:24:23 +0600

    modified login auth fauth for the password

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,15 +2,21 @@ import config from '../../config';
 import { UserModel } from '../User/user.model';
 import { TLoginUser } from './auth.interface';
 import { createToken } from './auth.utils';
-
+import bcrypt from 'bcrypt';
 const loginUser = async (payload: TLoginUser) => {
-  const user = await UserModel.findOne({ email: payload.email });
+  const user = await UserModel.findOne({ email: payload.email }).select(
+    '+password',
+  );
 
   if (!user) {
-    throw new Error('User is not exists!');
+    throw new Error('User does not exist!');
   }
 
-  console.log(user);
+  const isMatch = await bcrypt.compare(payload.password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Incorrect password!');
+  }
 
   const jwtPayload = {
     userId: user.id,
